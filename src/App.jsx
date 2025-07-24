12367742879b8435cdd9b167ad3af21ae4b7c89c
loppam <lolade132@gmail.com>
2025-07-25 00:08:40 +0100

    Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-// eslint-disable-next-line no-unused-vars
 import { AnimatePresence, motion } from "framer-motion";
 import "./App.css";
 
